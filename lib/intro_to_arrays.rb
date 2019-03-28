16f7f0d21a2e6ba920468eb1a86eae5531a88bec
Jason  Montes <jasone.montes@gmail.com>
2019-03-28 05:14:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,7 +13,7 @@ end
 def last_element(array)
   array[-1]
 end
-def first_element_with_array_method(array
+def first_element_with_array_method(array)
 array.first
 end
 def last_element_with_arrsy_method(array)
