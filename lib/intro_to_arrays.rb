912694b75573940085e35af9fe8aaffd32a268d9
Jason  Montes <jasone.montes@gmail.com>
2019-03-28 05:03:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,7 +1,6 @@
 def  instantiate_new_array
   empty_array = [ ]
 end 
-  
   def array_with_two_elements
     lovers = ("jason", "ashley")
   end 
@@ -13,19 +12,16 @@ end
 def third_element(array)
 array[2]
 end 
-    
-    def last_element(array)
-      array[-1]
-    end
-    
-    def first_element_with_array_method(array)
-      array.first
-    end
-    
-    def last_element_with_arrsy_method(array)
-      array.last 
-       
-      def length_of_array(array)
+def last_element(array)
+  array[-1]
+end
+def first_element_with_array_method(array)
+  array.first
+end
+def last_element_with_arrsy_method(array)
+  array.last 
+end
+def length_of_array(array)
         array.length_of_array
       end
       
