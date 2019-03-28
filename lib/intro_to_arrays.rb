60a00624c053cd9af818cd7b777d6d82f45f6256
Jason  Montes <jasone.montes@gmail.com>
2019-03-28 05:16:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,7 +17,7 @@ def first_element_with_array_method(array)
   array.first
 end
 def last_element_with_array_methods(array)
- array.last
+  array.last
 end
 def length_of_array(array)
   array.length
